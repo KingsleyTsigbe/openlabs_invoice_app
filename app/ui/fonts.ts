27da32ca04e49dd98115a49c3d239cb17157c5c7
MHARN LYK HILLS <kingsley Tsigbe>
2024-07-29 16:32:44 -0400

    Added database

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana,  Playwrite_AU_NSW } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
-//export const lusitana = Lusitana({weight:["400", "700"], subsets: ['latin'] });
+export const lusitana = Lusitana({weight: ['400', '700'], subsets: ['latin'] });
 
-//export const playwrite = Playwrite_AU_NSW({ weight: ['400', '100'] });
+export const playwrite = Playwrite_AU_NSW({weight: ['400', '100'], });
